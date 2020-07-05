a625cac1f790e522318a5505dd1092ee6fbd658f
Marek Ciesla <ciesla.marek@gmail.com>
2020-07-05 10:50:08 +0200

    refactor: rename class to DogRestApplication

--- src/main/java/com/udacity/dogrestapi/DogRestApplication.java
+++ src/main/java/com/udacity/dogrestapi/DogRestApplication.java
@@ -1,13 +1,13 @@
-package com.udacity.bootstrap;
+package com.udacity.dogrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootstrapApplication {
+public class DogRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootstrapApplication.class, args);
+		SpringApplication.run(DogRestApplication.class, args);
 	}
 
 }
